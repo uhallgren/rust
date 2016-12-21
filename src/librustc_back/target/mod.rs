2e1ec70abc9b509e0c4de860ad067a2878517c61
Ulf Hallgren <ulf.hallgren@teliacompany.com>
2016-12-21 23:02:47 +0100

    Added missing comma.

--- src/librustc_back/target/mod.rs
+++ src/librustc_back/target/mod.rs
@@ -209,7 +209,7 @@ supported_targets! {
     ("thumbv7em-none-eabihf", thumbv7em_none_eabihf),
 
     ("armv7-openwrt-linux-uclibc", armv7_openwrt_linux_uclibc),
-    ("mips-openwrt-linux-uclibc", mips_openwrt_linux_uclibc)
+    ("mips-openwrt-linux-uclibc", mips_openwrt_linux_uclibc),
 }
 
 /// Everything `rustc` knows about how to compile for a specific target.
